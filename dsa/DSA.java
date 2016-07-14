79c2500db59c0c9047eb23c775a9331aa399a7b3
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2016-07-14 10:41:47 +0200

    Also consider cost when choosing next state

--- dsa/DSA.java
+++ dsa/DSA.java
@@ -212,24 +212,31 @@ public abstract class DSA implements Solver {
 	private void selectNextValue(NodeVariable x, ArrayList<NodeVariable> variables) {
 
 		try {
+			double oldCost = cop.actualValue();
 			int oldState = x.getStateIndex();
 			int oldConflicts = getNumberOfConflicts(x);
 			logMessage(String.format("[% 5d] oldState = %d", x.getId(), oldState));
 			logMessage(String.format("[% 5d] oldConflicts = %d", x.getId(), oldConflicts));
+			logMessage(String.format("[% 5d] oldCost = %f", x.getId(), oldCost));
 			int bestState = oldState, delta = 0, bestConflicts = oldConflicts;
+			double bestCost = oldCost;
 
 			for (int newState = 0; newState < x.size(); newState++)
 				if (oldState != newState) {
 					x.setStateIndex(newState);
 					int newConflicts = getNumberOfConflicts(x);
+					double newCost = cop.actualValue();
 					logMessage(String.format("[% 5d] newState = %d", x.getId(), newState));
 					logMessage(String.format("[% 5d] newConflicts = %d", x.getId(), newConflicts));
-					if (newConflicts <= bestConflicts) {
 						bestConflicts = newConflicts;
+					if (newConflicts <= bestConflicts && newCost <= bestCost) {
 						bestState = newState;
+						bestConflicts = newConflicts;
+						bestCost = newCost;
 						delta = oldConflicts - bestConflicts;
-						logMessage(String.format("[% 5d] bestConflicts <- %d", x.getId(), bestConflicts));
 						logMessage(String.format("[% 5d] bestState <- %d", x.getId(), bestState));
+						logMessage(String.format("[% 5d] bestConflicts <- %d", x.getId(), bestConflicts));
+						logMessage(String.format("[% 5d] bestCost <- %f", x.getId(), bestCost));
 						logMessage(String.format("[% 5d] delta <- %d", x.getId(), delta));
 					}
 				}
